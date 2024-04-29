b343c76bd3cdcf298b9abd32b06593e018708dec
jcguu95 <jcguu95@gmail.com>
2024-04-28 22:36:00 -0400

    Make it easier to read.

--- Code/object.h
+++ Code/object.h
@@ -6,39 +6,29 @@
 #include <assert.h>
 #include <stdint.h>
 
-typedef struct header *object;
-
-typedef struct rack *rack;
-
-struct header
-{
+struct header {
   object class;
   rack rack;
 };
 
-struct rack
-{
+struct rack {
   int stamp;
   object slot_definitions;
 };
+
+typedef struct header *object;
+typedef struct rack *rack;
 
 #define DEFINE_CLASS(name) \
   static struct header name ## _header; object name = &name ## _header
 
 extern object make_object(void);
-
 extern rack make_rack(size_t size);
-
 extern object class_of(object obj);
-
 extern void set_class_of(object obj, object class);
-
 extern rack rack_of(object obj);
-
 extern void set_rack_of(object obj, rack r);
-
 extern size_t header_size;
-
 extern size_t rack_size;
 
 #endif
